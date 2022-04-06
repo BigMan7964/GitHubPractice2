214f8592eda830e225e375322a27f508f6d25d9e
Van Carton <vcarton2484@stu.d214.org>
2022-04-06 14:34:16 -0500

    hiya XD :D

--- GitHubPractice2/ViewController.swift
+++ GitHubPractice2/ViewController.swift
@@ -8,15 +8,7 @@
 import UIKit
 
 class ViewController: UIViewController, UITableViewDataSource {
-    func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
-        <#code#>
-    }
-    
-    func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
-        <#code#>
-    }
-    
-    
+
  
     
     @IBOutlet weak var tableView: UITableView!
@@ -28,7 +20,15 @@ class ViewController: UIViewController, UITableViewDataSource {
       
         
     }
-
+    func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
+        <#code#>
+    }
+    
+    func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
+        <#code#>
+    }
+    
+    
 
 }
 
